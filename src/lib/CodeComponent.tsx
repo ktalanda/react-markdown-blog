70fc313d7893b3911c7361915a5d212f456f94e0
Kamil Talanda <kamil0916@gmail.com>
2025-09-17 22:26:33 +0200

    Fix CodeComponent

--- src/lib/CodeComponent.tsx
+++ src/lib/CodeComponent.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type { JSX } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CodeComponent = (props: any): JSX.Element => {
